cfad733e2294d24d82fdfe2040ab7ceb43200b43
tmfadhli12 <thoriqmf123@gmail.com>
2025-02-24 14:34:32 +0700

    update insert model

---
+++ app/Models/FormPenilaian.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FormPenilaian extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/PenilaianRubrik.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PenilaianRubrik extends Model
+{
+    use HasFactory;
+}
