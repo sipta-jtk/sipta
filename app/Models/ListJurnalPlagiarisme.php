496c7ca092a1ca22a36aaabe1cd62d654dfcf393
RezaMaulana <reza.maulana.tif422@polban.ac.id>
2025-02-24 15:40:28 +0700

    update model konfirmasi, kota, label, lableDokumen, listjurnalplagiarisme

--- app/Models/ListJurnalPlagiarisme.php
+++ app/Models/ListJurnalPlagiarisme.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class ListJurnalPlagiarisme extends Model
 {
     use HasFactory;
+
+    protected $table = 'list_jurnal_plagiarisme'; // Nama tabel
+
+    protected $primaryKey = 'id_jurnal'; // Primary key
+
+    public $timestamps = false; // Tidak menggunakan created_at & updated_at
+
+    protected $fillable = [
+        'link_jurnal',
+        'judul',
+        'persentase_kemunculan'
+    ];
 }
