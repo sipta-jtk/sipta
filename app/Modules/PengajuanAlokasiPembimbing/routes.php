64ba1dfb812cbac8ea000f71bad68ca4cc3d2800
Matharrr <matharrizqi@gmail.com>
2025-03-03 12:56:10 +0700

    Data tabel, next backend

--- app/Modules/PengajuanAlokasiPembimbing/routes.php
+++ app/Modules/PengajuanAlokasiPembimbing/routes.php
@@ -1,10 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Modules\PengajuanAlokasiPembimbing\Controllers\PengajuanAlokasiPembimbingController;
+use App\Modules\PengajuanAlokasiPembimbing\Controllers\AlokasiPembimbingController;
 
 Route::group(['prefix' => 'PengajuanAlokasiPembimbing'], function () {
-    Route::group(['prefix' => 'kesediaan-membimbing'], function () {
-        Route::get('/minat-bidang', [PengajuanAlokasiPembimbingController::class, 'view_minatTopik']);
-    });
+    Route::get('/alokasi-pembimbing', [AlokasiPembimbingController::class, 'index']);
 });
