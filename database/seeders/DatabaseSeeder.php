1a3fb802aedab0e86da3fae52c75b807a5760b3f
kittendev <Dafaalfarizki33.DA@gmail.com>
2025-03-07 21:31:47 +0700

    [Chore] Fix Seeder for RuangFasilitas, RubrikPenilaian, SubKategori, and User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,10 @@ class DatabaseSeeder extends Seeder
         $this->call(NotifikasiKirimSeeder::class);
         $this->call(NotifikasiSeeder::class);
         $this->call(PengajuanPembimbingSeeder::class);
+        $this->call(RuangFasilitasSeeder::class);
+        $this->call(RubrikPenilaianSeeder::class);
+        $this->call(SubKategoriSeeder::class);
+        $this->call(UserSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
