21dd05ca231c29e24f92b1fb37c603273bd19093
najibalimudinn <najibaf147@gmail.com>
2025-03-08 20:54:33 +0700

    [fix] seeder error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // First batch: Base tables with no dependencies
         $this->call([
-            // Base tables with no dependencies
             UserSeeder::class,
             GedungSeeder::class,
             FasilitasSeeder::class,
@@ -23,50 +22,66 @@ class DatabaseSeeder extends Seeder
             PeriodePengajuanSeeder::class,
             SubKategoriSeeder::class,
             AmbangBatasSeeder::class,
+        ]);
 
-            // First level dependencies
+        // Second batch: First level dependencies
+        $this->call([
             DosenSeeder::class, // depends on User, KBK
             RuanganSeeder::class, // depends on Gedung
             KotaSeeder::class, // depends on Bidang
+        ]);
 
-            // Second level dependencies
+        // Third batch: Second level dependencies
+        $this->call([
             MahasiswaSeeder::class, // depends on User, Prodi, Kota
             KaprodiSeeder::class, // depends on Dosen, Prodi
             JadwalDosenPembimbingSeeder::class, // depends on Dosen
             KetertarikanBidangSeeder::class, // depends on Dosen, Bidang
             RuangFasilitasSeeder::class, // depends on Fasilitas, Ruangan
             LabelSeeder::class, // depends on Kota
+        ]);
 
-            // Higher level dependencies
+        // Fourth batch: Higher level dependencies
+        $this->call([
             PengajuanPembimbingSeeder::class, // depends on Kota
             PrioritasPembimbingSeeder::class, // depends on PengajuanPembimbing, Dosen
             AlokasiPembimbingSeeder::class, // depends on PengajuanPembimbing, Dosen
             PengajuanPisahKotaSeeder::class, // depends on Mahasiswa, Kota
+        ]);
 
-            // Assessment related
+        // Fifth batch: Assessment related
+        $this->call([
             FormPenilaianSeeder::class, // depends on Dosen
             KategoriPenilaianSeeder::class, // depends on FormPenilaian
             RubrikPenilaianSeeder::class, // depends on KategoriPenilaian
             PenilaianRubrikSeeder::class, // depends on Mahasiswa, Dosen, RubrikPenilaian
             PenilaianKategoriSeeder::class, // depends on Mahasiswa
+        ]);
 
-            // Document related
+        // Sixth batch: Document related
+        $this->call([
             DokumenSeeder::class, // depends on Kota, Label, SubKategori, User
             ListJurnalPlagiarismeSeeder::class, // no dependencies
             ListKalimatPlagiarismeSeeder::class, // depends on Dokumen, ListJurnalPlagiarisme
             MahasiswaDosenDokumenSeeder::class, // depends on Dosen, Mahasiswa, Dokumen
+        ]);
 
-            // Scheduling related
+        // Seventh batch: Scheduling related
+        $this->call([
             PenjadwalanSeeder::class, // depends on Ruangan, Kota, Dosen
             KehadiranSeeder::class, // depends on Penjadwalan, User
             KonfirmasiSeeder::class, // depends on Penjadwalan, Dosen
+        ]);
 
-            // Notification related
+        // Eighth batch: Notification related
+        $this->call([
             NotifikasiSeeder::class, // no dependencies
             NotifikasiKirimSeeder::class, // depends on Notifikasi, User
             PreferensiNotifikasiSeeder::class, // depends on User
+        ]);
 
-            // Logs (should be last as they depend on many entities)
+        // Ninth batch: Logs (should be last as they depend on many entities)
+        $this->call([
             LogAktivitasSeeder::class, // depends on Kota, User, Dokumen
         ]);
     }
