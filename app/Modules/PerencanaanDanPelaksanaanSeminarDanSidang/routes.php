1709f92bdc4c2e9a05688dd0aa9fc9f94385c7aa
ferdiahmad3404@gmail.com <ferdiahmad3404@gmail.com>
2025-03-15 16:43:54 +0700

    [FIX] Deleting unused comment

--- app/Modules/PerencanaanDanPelaksanaanSeminarDanSidang/routes.php
+++ app/Modules/PerencanaanDanPelaksanaanSeminarDanSidang/routes.php
@@ -1,31 +1,5 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
-// use App\Modules\PerencanaanDanPelaksanaanSeminarDanSidang\Controllers\VerifikasiBerkasSeminarController as BerkasSeminar;
-// use App\Modules\PerencanaanDanPelaksanaanSeminarDanSidang\Controllers\VerifikasiBerkasSidangController as BerkasSidang;
-
-// Route::group(['prefix' => 'kelola-pengajuan', 'as' => 'kelola.'], function () {
-//     Route::group(['prefix' => 'berkas-seminar-3', 'as' => 'seminar.'], function () {
-//         Route::get('/', [BerkasSeminar::class, 'index'])->name('list');
-//         Route::get('/ditolak', [BerkasSeminar::class, 'pengajuanDitolak'])->name('ditolak');
-//         Route::get('/diterima', [BerkasSeminar::class, 'pengajuanDiterima'])->name('diterima');
-//         Route::get('/detail/{id}', [BerkasSeminar::class, 'show'])->name('detail');
-        
-//         // Route untuk proses verifikasi (setuju/tolak)
-//         Route::post('/verifikasi/{id}', [BerkasSeminar::class, 'verifikasi'])->name('verifikasi');
-//     });
-
-//     Route::group(['prefix' => 'berkas-sidang-akhir', 'as' => 'sidang.'], function () {
-//         Route::get('/', [BerkasSidang::class, 'index'])->name('list');
-//         Route::get('/ditolak', [BerkasSidang::class, 'pengajuanDitolak'])->name('ditolak');
-//         Route::get('/diterima', [BerkasSidang::class, 'pengajuanDiterima'])->name('diterima');
-//         Route::get('/detail/{id}', [BerkasSidang::class, 'show'])->name('detail');
-        
-//         // Route untuk proses verifikasi (setuju/tolak)
-//         Route::post('/verifikasi/{id}', [BerkasSidang::class, 'verifikasi'])->name('verifikasi');
-//     });
-// });
-
 use Illuminate\Support\Facades\Route;
 use App\Modules\PerencanaanDanPelaksanaanSeminarDanSidang\Controllers\VerifikasiBerkasController;
 
