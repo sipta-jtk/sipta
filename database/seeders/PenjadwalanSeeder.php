2dd8d3f0bd62571c772cfb511b3c9f641c6800ee
FahriYuwan <130884349+FahriYuwan@users.noreply.github.com>
2025-03-17 15:47:50 +0700

    Update PenjadwalanSeeder.php

--- database/seeders/PenjadwalanSeeder.php
+++ database/seeders/PenjadwalanSeeder.php
@@ -25,27 +25,59 @@ class PenjadwalanSeeder extends Seeder
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         Penjadwalan::create([
-            'sesi' => '1', // Ubah Manual
-            'agenda' => 'seminar_1', // Ubah Manual
-            'id_ruangan' => '1', // Ubah Manual
-            'tanggal' => Carbon::now(), // Ubah Manual
-            'id_kota' => '1', // Ubah Manual
-            'nip' => '197312271999031003', // Ubah Manual
-            'start' => Carbon::now(),
-            'end' => Carbon::create(2025, 6, 1, 14, 30, 0)
-
+            'sesi' => '1', 
+            'agenda' => 'seminar_1', 
+            'id_ruangan' => '1', 
+            'tanggal' => Carbon::create(2025, 3, 17), // Tanggal 17 Maret 2025
+            'id_kota' => '2', 
+            'nip' => '197312271999031003', 
+            'start' => Carbon::create(2025, 3, 17, 8, 0, 0), // 17/03/2025 8:00:00
+            'end' => Carbon::create(2025, 3, 17, 10, 0, 0) // 17/03/2025 10:00:00
         ]);
 
         Penjadwalan::create([
-            'sesi' => '2', // Ubah Manual
-            'agenda' => 'seminar_2', // Ubah Manual
-            'id_ruangan' => '2', // Ubah Manual
-            'tanggal' => Carbon::now(), // Ubah Manual
-            'id_kota' => '2', // Ubah Manual
-            'nip' => '198502102015042001', // Ubah Manual
-            'start' => Carbon::now(),
-            'end' => Carbon::create(2025, 5, 1, 13, 0, 0)
-
+            'sesi' => '2', 
+            'agenda' => 'seminar_2', 
+            'id_ruangan' => '2', 
+            'tanggal' => Carbon::create(2025, 3, 17), // Tanggal 17 Maret 2025
+            'id_kota' => '6', 
+            'nip' => '196810141993032002', 
+            'start' => Carbon::create(2025, 3, 17, 10, 30, 0), 
+            'end' => Carbon::create(2025, 3, 17, 12, 30, 0) 
         ]);
+
+        Penjadwalan::create([
+            'sesi' => '3', 
+            'agenda' => 'seminar_3', 
+            'id_ruangan' => '3', 
+            'tanggal' => Carbon::create(2025, 3, 17), // Tanggal 17 Maret 2025
+            'id_kota' => '1', 
+            'nip' => '197201061999031002', 
+            'start' => Carbon::create(2025, 3, 17, 13, 30, 0), 
+            'end' => Carbon::create(2025, 3, 17, 15, 30, 0) 
+        ]);
+
+        Penjadwalan::create([
+            'sesi' => '4', 
+            'agenda' => 'sidang', 
+            'id_ruangan' => '4', 
+            'tanggal' => Carbon::create(2025, 3, 18), // Tanggal 18 Maret 2025
+            'id_kota' => '5', 
+            'nip' => '196012261992031001', 
+            'start' => Carbon::create(2025, 3, 18, 10, 00, 0), 
+            'end' => Carbon::create(2025, 3, 18, 12, 00, 0) 
+        ]);
+
+        Penjadwalan::create([
+            'sesi' => '2', 
+            'agenda' => 'sidang', 
+            'id_ruangan' => '5', 
+            'tanggal' => Carbon::create(2025, 3, 18), // Tanggal 18 Maret 2025
+            'id_kota' => '9', 
+            'nip' => '196101141992021001', 
+            'start' => Carbon::create(2025, 3, 18, 13, 00, 0), 
+            'end' => Carbon::create(2025, 3, 18, 15, 00, 0) 
+        ]);
+
     }
 }
