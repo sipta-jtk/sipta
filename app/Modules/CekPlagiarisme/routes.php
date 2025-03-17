ad3bca3528ad83c3bfba889b109e4dca15abb64c
Muhammad Azhar <117720953+MuhammadAzhar9@users.noreply.github.com>
2025-03-17 15:15:16 +0700

    [Fix] : fix routes

--- app/Modules/CekPlagiarisme/routes.php
+++ app/Modules/CekPlagiarisme/routes.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Modules\CekPlagiarisme\Controllers\CekPlagiarismeController;
-use App\Modules\CekPlagiarisme\Controllers\CekPlagiarismeDetailController;
+use App\Modules\CekPlagiarisme\Controllers\AmbangBatasController;
 
-
-Route::get('/cek-plagiarisme', [CekPlagiarismeController::class, 'index']);
-Route::get('/cek-plagiarisme/{id}', [CekPlagiarismeDetailController::class, 'show'])->name('plagiarism.detail');
-Route::get('/penentuan-ambang-batas', [CekPlagiarismeController::class, 'PenentuanAmbangBatas']);
-Route::get('/cek-plagiarisme-catatan', [CekPlagiarismeDetailController::class, 'povMahasiswa'])->name('povMahasiswa');
+Route::get('/api/cek-plagiarisme', [cekplagiarismeController::class, 'getData']);
+Route::get('/cek-plagiarisme/{id}', [CekPlagiarismeController::class, 'show'])->name('plagiarism.detail');
+Route::get('/api/ambang-batas', [AmbangBatasController::class, 'getData']);
+Route::post('/api/ambang-batas', [AmbangBatasController::class, 'store']);
+// Route::middleware('auth')->post('/api/ambang-batas', [AmbangBatasController::class, 'store'])->name('cekplagiarisme.index');
+Route::post('/cekplagiarisme/process', [CekPlagiarismeController::class, 'process'])->name('cekplagiarisme.process');
 
 Route::get('/penentuan-ambang-batas', function () {
     return view('CekPlagiarisme.views.PenentuanAmbangBatas');
@@ -16,4 +17,3 @@ Route::get('/penentuan-ambang-batas', function () {
 Route::get('/cek-plagiarisme', function () {
     return view('CekPlagiarisme.views.DaftarDokumen');
 });
-
