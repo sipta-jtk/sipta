4f342c7a5b37ad7bcfe640d961734cfee20aa955
Muhammad Azhar <117720953+MuhammadAzhar9@users.noreply.github.com>
2025-03-17 15:13:03 +0700

    [Fix] : fix error routes

--- app/Modules/CekPlagiarisme/routes.php
+++ app/Modules/CekPlagiarisme/routes.php
@@ -4,16 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Modules\CekPlagiarisme\Controllers\CekPlagiarismeController;
 use App\Modules\CekPlagiarisme\Controllers\CekPlagiarismeDetailController;
 
-<<<<<<< HEAD
+
 Route::get('/cek-plagiarisme', [CekPlagiarismeController::class, 'index']);
 Route::get('/cek-plagiarisme/{id}', [CekPlagiarismeDetailController::class, 'show'])->name('plagiarism.detail');
 Route::get('/penentuan-ambang-batas', [CekPlagiarismeController::class, 'PenentuanAmbangBatas']);
 Route::get('/cek-plagiarisme-catatan', [CekPlagiarismeDetailController::class, 'povMahasiswa'])->name('povMahasiswa');
-=======
+
 Route::get('/penentuan-ambang-batas', function () {
     return view('CekPlagiarisme.views.PenentuanAmbangBatas');
 });
 Route::get('/cek-plagiarisme', function () {
     return view('CekPlagiarisme.views.DaftarDokumen');
 });
->>>>>>> cb451d45482bce69a240cb275e9798d704065c57
+
