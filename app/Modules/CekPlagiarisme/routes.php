c4a7e3e3e6992386db963a274c5a3f0bf96d0e8b
Adinda <adindafauziapus@gmail.com>
2025-03-18 01:49:42 +0700

    Resolved merge conflict routes

--- app/Modules/CekPlagiarisme/routes.php
+++ app/Modules/CekPlagiarisme/routes.php
@@ -2,11 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Modules\CekPlagiarisme\Controllers\CekPlagiarismeController;
-<<<<<<< HEAD
-use App\Modules\CekPlagiarisme\Controllers\CekPlagiarismeDetailController;
-=======
->>>>>>> ad3bca3528ad83c3bfba889b109e4dca15abb64c
 use App\Modules\CekPlagiarisme\Controllers\AmbangBatasController;
+use App\Modules\CekPlagiarisme\Controllers\CekPlagiarismeDetailController;
 
 Route::get('/api/cek-plagiarisme', [cekplagiarismeController::class, 'getData']);
 Route::get('/cek-plagiarisme/{id}', [CekPlagiarismeController::class, 'show'])->name('plagiarism.detail');
@@ -15,16 +12,12 @@ Route::post('/api/ambang-batas', [AmbangBatasController::class, 'store']);
 // Route::middleware('auth')->post('/api/ambang-batas', [AmbangBatasController::class, 'store'])->name('cekplagiarisme.index');
 Route::post('/cekplagiarisme/process', [CekPlagiarismeController::class, 'process'])->name('cekplagiarisme.process');
 
-<<<<<<< HEAD
-Route::get('/cek-plagiarisme', [CekPlagiarismeController::class, 'index']);
-Route::get('/cek-plagiarisme/{id}', [CekPlagiarismeDetailController::class, 'show'])->name('plagiarism.detail');
-Route::get('/penentuan-ambang-batas', [CekPlagiarismeController::class, 'PenentuanAmbangBatas']);
-Route::get('/cek-plagiarisme-catatan', [CekPlagiarismeDetailController::class, 'povMahasiswa'])->name('povMahasiswa');
-=======
->>>>>>> ad3bca3528ad83c3bfba889b109e4dca15abb64c
 Route::get('/penentuan-ambang-batas', function () {
     return view('CekPlagiarisme.views.PenentuanAmbangBatas');
 });
 Route::get('/cek-plagiarisme', function () {
     return view('CekPlagiarisme.views.DaftarDokumen');
 });
+
+Route::get('/cek-plagiarisme/{id}', [CekPlagiarismeDetailController::class, 'show'])->name('plagiarism.detail');
+Route::get('/cek-plagiarisme-catatan', [CekPlagiarismeDetailController::class, 'povMahasiswa'])->name('povMahasiswa');
