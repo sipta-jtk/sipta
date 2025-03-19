205ce752db3c6c6687c5b43758c64668b7d243d2
hanifzndn <mohdhanif1610@gmail.com>
2025-03-19 18:02:22 +0700

    [Feature]update kelola jurursan

--- app/Modules/UserManagement/routes.php
+++ app/Modules/UserManagement/routes.php
@@ -84,10 +84,6 @@ Route::get('/kelola-kbk', [KBKController::class, 'index'])->name('kelola-kbk');
 Route::post('/kelola-kbk', [KBKController::class, 'store'])->name('kelola-kbk.store');
 Route::post('/kelola-kbk/update/{id}', [KBKController::class, 'update'])->name('kelola-kbk.update');
 Route::delete('/kelola-kbk/{id}', [KBKController::class, 'destroy'])->name('kelola-kbk.destroy');
-//
-
-// Route::get('program-studi', [ProgramStudiController::class, 'index'])->name('program-studi.index');
-//
 
 Route::get('program-studi', [ProgramStudiController::class, 'index'])->name('program-studi.index');
 Route::get('program-studi/create', [ProgramStudiController::class, 'create'])->name('program-studi.create');
