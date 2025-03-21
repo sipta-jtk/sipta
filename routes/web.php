96d09be2f057c9dc61b5c02e3bcd61b18bb9db2e
RahmaDivina <rahma.divina.tif422@polban.ac.id>
2025-03-21 21:39:40 +0700

    Selesaikan konflik di config/adminlte.php dan routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 $modulesPath = base_path('app/Modules');
-$prefix = env('PREFIX_URL', 'sipta');
 
-// Load routes from modules dynamically
-foreach (scandir($modulesPath) as $module) {
-    $routesFile = "{$modulesPath}/{$module}/routes.php";
+if (is_dir($modulesPath)) {
+    foreach (scandir($modulesPath) as $module) {
+        if ($module === '.' || $module === '..') {
+            continue;
+        }
+        $routesFile = "{$modulesPath}/{$module}/routes.php";
 
-    if (is_file($routesFile)) {
-        Route::prefix($prefix)->group(function () use ($routesFile) {
-            require $routesFile;
-        });
+        if (is_file($routesFile)) {
+            require $routesFile; 
+        }
     }
 }
 
-// Default route for the homepage
 Route::get('/', function () {
     return view('welcome');
 })->middleware('auth'); // Hanya user login yang bisa akses
@@ -36,4 +36,3 @@ Route::post('/logout', function () {
     auth()->logout();
     return redirect('/login');
 })->name('logout');
-
