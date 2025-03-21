e86b5db3635e355c1986f32b9db3c041d3cabaee
Adinda <adindafauziapus@gmail.com>
2025-03-21 23:23:42 +0700

    update feature catatan

--- app/Modules/CekPlagiarisme/Controllers/CekPlagiarismeDetailController.php
+++ app/Modules/CekPlagiarisme/Controllers/CekPlagiarismeDetailController.php
@@ -9,6 +9,7 @@ use App\Models\Dosen;
 use App\Models\ReviewDosenPembimbing;
 use App\Models\ListJurnalPlagiarisme;
 use App\Models\ListKalimatPlagiarisme;
+use App\Models\AlokasiDosen;
 
 class CekPlagiarismeDetailController extends Controller
 {
@@ -18,19 +19,22 @@ class CekPlagiarismeDetailController extends Controller
 
         // Mengambil dokumen berdasarkan ID
         $dokumen = Dokumen::find($id);
-        
+
         // Mengambil semua review (catatan) yang terkait dengan dokumen
-        $catatan = ReviewDosenPembimbing::with('dosen')->where('id_dokumen', $id)->get();
-        
+        $catatan = ReviewDosenPembimbing::with('dosen.user')->where('id_dokumen', $id)->get();
+
         // Mengambil kalimat plagiat yang berelasi dengan dokumen dan jurnal
         $sumberPlagiarisme = ListKalimatPlagiarisme::with('listJurnalPlagiarisme') // Menggunakan relasi yang benar
             ->where('id_dokumen', $id)
             ->get();
-    
+
+        // Mengambil data alokasi dosen yang statusnya 'fix' dan mengirimkan ke view
+        $alokasiDosen = AlokasiDosen::all();
+
         // Mengirimkan data ke view
-        return view('CekPlagiarisme.views.detail', compact('dokumen', 'catatan', 'sumberPlagiarisme')); // Tambahkan 'sumberPlagiarisme'
+        return view('CekPlagiarisme.views.detail', compact('dokumen', 'catatan', 'sumberPlagiarisme', 'alokasiDosen')); // Tambahkan 'sumberPlagiarisme'
     }
-    
+
 
     public function PenentuanAmbangBatas(): View
     {
