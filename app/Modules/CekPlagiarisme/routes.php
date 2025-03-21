97f4f1a0e2ea6c19271ddbadea0f317049027497
agapehhhhhhh <114454472+agapehhhhhhh@users.noreply.github.com>
2025-03-21 10:49:42 +0700

    [UPDATE] add routes for plagiarism detail view and student notes; remove unused route

--- app/Modules/CekPlagiarisme/routes.php
+++ app/Modules/CekPlagiarisme/routes.php
@@ -12,9 +12,8 @@ Route::post('/api/ambang-batas', [AmbangBatasController::class, 'store']);
 // Route::middleware('auth')->post('/api/ambang-batas', [AmbangBatasController::class, 'store'])->name('cekplagiarisme.index');
 Route::post('/cekplagiarisme/process', [CekPlagiarismeController::class, 'process'])->name('cekplagiarisme.process');
 
-Route::get('/penentuan-ambang-batas', function () {
-    return view('CekPlagiarisme.views.PenentuanAmbangBatas');
-});
 Route::get('/cek-plagiarisme', function () {
     return view('CekPlagiarisme.views.DaftarDokumen');
 });
+Route::get('/cek-plagiarisme/{id}', [CekPlagiarismeDetailController::class, 'show'])->name('plagiarism.detail');
+Route::get('/cek-plagiarisme-catatan', [CekPlagiarismeDetailController::class, 'povMahasiswa'])->name('povMahasiswa');
