8b0e8bf14dc9d3d762e13088329fc793da93ae1e
Muhammad Azhar <117720953+MuhammadAzhar9@users.noreply.github.com>
2025-03-22 00:12:33 +0700

    [Feature] : role akses untuk filter hanya bisa diliat oleh dosen

--- app/Modules/CekPlagiarisme/routes.php
+++ app/Modules/CekPlagiarisme/routes.php
@@ -6,6 +6,7 @@ use App\Modules\CekPlagiarisme\Controllers\AmbangBatasController;
 use App\Modules\CekPlagiarisme\Controllers\CekPlagiarismeDetailController;
 
 Route::get('/api/cek-plagiarisme', [cekplagiarismeController::class, 'getData']);
+Route::middleware(['auth', 'can:dosen'])->get('/api/kotas', [CekPlagiarismeController::class, 'getKota']);
 Route::get('/cek-plagiarisme/{id}/detail-dokumen', [CekPlagiarismeDetailController::class, 'show'])->name('plagiarism.detail');
 Route::get('/cek-plagiarisme-catatan', [CekPlagiarismeDetailController::class, 'povMahasiswa'])->name('povMahasiswa');
 Route::post('/cekplagiarisme/process', [CekPlagiarismeController::class, 'process'])->name('cekplagiarisme.process');
