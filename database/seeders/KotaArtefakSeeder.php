aac68e459c106d0ba227852770dedd45f2001410
ZahranAn <123435005+ZahranAn@users.noreply.github.com>
2025-03-24 09:55:57 +0700

    seeder kota artefak

--- database/seeders/KotaArtefakSeeder.php
+++ database/seeders/KotaArtefakSeeder.php
@@ -14,53 +14,47 @@ class KotaArtefakSeeder extends Seeder
     
     public function run(): void
     {
-        KotaArtefak::create([
-            'id_kota' => '2',
-            'id_artefak' => '7',
-            'file_pengumpulan' => 'faef',
-            'waktu_pengumpulan' => '2024-05-30 23:59:00',
-        ]);
 
         KotaArtefak::create([
             'id_kota' => '2',
             'id_artefak' => '8',
             'file_pengumpulan' => 'garerg',
-            'waktu_pengumpulan' => '2024-05-30 23:59:00',
+            'waktu_pengumpulan' => '2025-03-24 09:36:17',
         ]);
 
         KotaArtefak::create([
             'id_kota' => '2',
             'id_artefak' => '9',
             'file_pengumpulan' => 'gareg',
-            'waktu_pengumpulan' => '2024-05-30 23:59:00',
+            'waktu_pengumpulan' => '2025-03-24 09:36:17',
         ]);
 
         KotaArtefak::create([
             'id_kota' => '2',
             'id_artefak' => '10',
             'file_pengumpulan' => 'gagareg',
-            'waktu_pengumpulan' => '2024-05-30 23:59:00',
+            'waktu_pengumpulan' => '2025-03-24 09:36:17',
         ]);
 
         KotaArtefak::create([
             'id_kota' => '2',
             'id_artefak' => '11',
             'file_pengumpulan' => 'gfawfareg',
-            'waktu_pengumpulan' => '2024-05-30 23:59:00',
+            'waktu_pengumpulan' => '2025-03-24 09:36:17',
         ]);
 
         KotaArtefak::create([
             'id_kota' => '2',
             'id_artefak' => '12',
             'file_pengumpulan' => 'gargsfeg',
-            'waktu_pengumpulan' => '2024-05-30 23:59:00',
+            'waktu_pengumpulan' => '2025-03-24 09:36:17',
         ]);
 
         KotaArtefak::create([
             'id_kota' => '2',
             'id_artefak' => '13',
             'file_pengumpulan' => 'gaawfareg',
-            'waktu_pengumpulan' => '2024-05-30 23:59:00',
+            'waktu_pengumpulan' => '2025-03-24 09:36:17',
         ]);
 
     }
