9d06c94b8bdda41321a7ca0cb9dd6f0bc47dbc4f
ZahranAn <123435005+ZahranAn@users.noreply.github.com>
2025-03-24 11:39:51 +0700

    benerin id kota

--- app/Modules/DosenTabelPenilaian/views/view.blade.php
+++ app/Modules/DosenTabelPenilaian/views/view.blade.php
@@ -41,7 +41,7 @@
                                         Lihat Nilai
                                     </a>
                                     @elseif ($item['status_penilaian'] === 'draf')
-                                    <a href="{{ route('pengisian.nilai', ['id' => $item['id'], 'kota' => $item['kota']]) }}"
+                                    <a href="{{ route('pengisian.nilai', ['id' => $item['id'], 'kota' => $item['id_kota']]) }}"
                                         class="btn btn-warning btn-sm">
                                         Edit Nilai
                                     </a>
@@ -58,7 +58,7 @@
                                     </form>
                                     @endif
                                 @else
-                                <a href="{{ route('pengisian.nilai', ['id' => $item['id'], 'kota' => $item['kota']]) }}"
+                                <a href="{{ route('pengisian.nilai', ['id' => $item['id'], 'kota' => $item['id_kota']]) }}"
                                     class="btn btn-primary btn-sm"> 
                                     Isi Nilai
                                 </a>
