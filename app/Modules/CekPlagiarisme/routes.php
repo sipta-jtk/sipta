7a6e4f248348f52a45d205d8feabef1e3e9330f3
Dean <deanadrian644@gmail.com>
2025-03-24 10:38:04 +0700

    feature: plagiarism checking from django app

--- app/Modules/CekPlagiarisme/routes.php
+++ app/Modules/CekPlagiarisme/routes.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Modules\CekPlagiarisme\Controllers\CekPlagiarismeController;
 use App\Modules\CekPlagiarisme\Controllers\AmbangBatasController;
 use App\Modules\CekPlagiarisme\Controllers\CekPlagiarismeDetailController;
+use App\Modules\CekPlagiarisme\Controllers\PengecekanTugasAkhirController;
 
 Route::get('/api/cek-plagiarisme', [cekplagiarismeController::class, 'getData']);
 Route::middleware(['auth', 'can:dosen'])->get('/api/kotas', [CekPlagiarismeController::class, 'getKota']);
@@ -14,6 +15,13 @@ Route::post('/cekplagiarisme/process', [CekPlagiarismeController::class, 'proces
 Route::get('/cek-plagiarisme', function () {
     return view('CekPlagiarisme.views.DaftarDokumen');
 });
+
+
+Route::get('/cek-plagiarisme/cek-tugas-akhir', function () {
+    return view('CekPlagiarisme.views.PengecekanTugasAkhir');
+});
+
+Route::post('/cek-plagiarisme/cek-tugas-akhir', [PengecekanTugasAkhirController::class, 'cekTugasAkhir']);
 
 
 /**********************************
