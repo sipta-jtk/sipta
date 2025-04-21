be3b791ca2d3945de93c51e3718fa36406e547f3
rahzlavnia <sarah.ajjh69@gmail.com>
2025-04-21 11:43:22 +0700

    [fix] rollback changes in plagiarism module to align with staging version

--- app/Modules/CekPlagiarisme/routes.php
+++ app/Modules/CekPlagiarisme/routes.php
@@ -5,16 +5,17 @@ use App\Modules\CekPlagiarisme\Controllers\CekPlagiarismeController;
 use App\Modules\CekPlagiarisme\Controllers\AmbangBatasController;
 use App\Modules\CekPlagiarisme\Controllers\CekPlagiarismeDetailController;
 
+Route::middleware(['auth', 'can:dosen'])->get(
+    '/api/kotas',
+    [CekPlagiarismeController::class, 'getKota']
+);
 Route::get('/api/cek-plagiarisme', [cekplagiarismeController::class, 'getData']);
-Route::middleware(['auth', 'can:dosen'])->get('/api/kotas', [CekPlagiarismeController::class, 'getKota']);
 Route::get('/cek-plagiarisme/{id}/detail-dokumen', [CekPlagiarismeDetailController::class, 'show'])->name('plagiarism.detail');
-Route::get('/cek-plagiarisme-catatan', [CekPlagiarismeDetailController::class, 'povMahasiswa'])->name('povMahasiswa');
 Route::post('/cekplagiarisme/process', [CekPlagiarismeController::class, 'process'])->name('cekplagiarisme.process');
 
 Route::get('/cek-plagiarisme', function () {
     return view('CekPlagiarisme.views.DaftarDokumen');
 });
-
 
 /**********************************
  * Penentuan Ambang Batas
@@ -25,4 +26,13 @@ Route::middleware(['auth', 'can:koordinator_ta'])->group(function () {
     });
     Route::get('/api/ambang-batas', [AmbangBatasController::class, 'getData']);
     Route::post('/api/ambang-batas', [AmbangBatasController::class, 'store']);
+});
+
+/**********************************
+ * Catatan oleh Dosen Pembimbing
+ ***********************************/
+Route::group(['prefix' => 'cek-plagiarisme', 'as' => 'cek-plagiarisme.', 'middleware' => ['auth', 'can:dosen']], function () {
+    Route::post('catatan-store/{id_dokumen}', [CekPlagiarismeDetailController::class, 'storeCatatan'])->name('catatan.store');
+    Route::put('catatan-store/{id_dokumen}/{id}', [CekPlagiarismeDetailController::class, 'updateCatatan'])->name('catatan.put');
+    Route::delete('catatan-store/{id_dokumen}/{id}', [CekPlagiarismeDetailController::class, 'deleteCatatan'])->name('catatan.delete');
 });
