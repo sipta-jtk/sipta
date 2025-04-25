36b990665c19d691de003345c601e254e700fec6
Daffabaisa <muhammad.daffa.tif422@polban.ac.id>
2025-04-25 14:26:31 +0700

    [fix] ui

--- app/Modules/KelolaPenilaianTA/views/monitoring-nilai-mahasiswa/monitoring_mahasiswa.blade.php
+++ app/Modules/KelolaPenilaianTA/views/monitoring-nilai-mahasiswa/monitoring_mahasiswa.blade.php
@@ -3,7 +3,8 @@
 @section('title', 'Monitoring Mahasiswa')
 
 @section('content_header')
-    <div class="container-fluid p-3">
+    <h1 class="mb-3">Informasi Penilaian Mahasiswa</h1>
+    <div>
         @component('KelolaPenilaianTA.views.components.breadcrumb', [
             'links' => [
                 ['url' => route('beranda.get'), 'label' => 'Home'],
@@ -11,80 +12,89 @@
             ]
         ])
         @endcomponent
-
-        <!-- Judul Halaman -->
-        <h1 class="mb-0">Informasi Penilaian Mahasiswa</h1>
     </div>
 @stop
 
 @section('content')
-    <div class="card-KoTA">
-        <h4>Informasi KoTA</h4>
-        <table class="table table-bordered w-50">
-            <tbody>
-                <tr>
-                    <th class="bg-dark text-white w-25 text-center p-2">Kode KoTA</th>
-                    <td class="w-75 p-2">{{ $kotaInfo->nama_kota }}</td>
-                </tr>
-                <tr>
-                    <th class="bg-dark text-white w-25 text-center p-2">Judul Topik</th>
-                    <td class="w-75 p-2">{{ $kotaInfo->judul_ta }}</td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="card">
+        <div class="card-header text-center">
+            <h3 class="card-title w-100">Informasi KoTA</h3>
+        </div>
+        <div class="card-body px-3 py-3">
+            <table class="table table-bordered w-50">
+                <tbody>
+                    <tr>
+                        <th class="bg-dark text-white w-25 text-center p-2">Kode KoTA</th>
+                        <td class="w-75 p-2">{{ $kotaInfo->nama_kota }}</td>
+                    </tr>
+                    <tr>
+                        <th class="bg-dark text-white w-25 text-center p-2">Judul Topik</th>
+                        <td class="w-75 p-2">{{ $kotaInfo->judul_ta }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 
-    <div class="card-Mahasiswa">
-        <h4>Data Mahasiswa</h4>
-        <table class="table table-bordered w-50">
-            <thead>
-                <tr class="bg-dark text-white" style="text-align: center;">
-                    <th class="bg-dark align-middle text-center w-25">NIM</th>
-                    <th class="bg-dark align-middle text-center w-75">Nama</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($mahasiswaList as $mahasiswa)
-                    <tr>
-                        <td>{{ $mahasiswa->nim }}</td>
-                        <td>{{ $mahasiswa->user->nama }}</td>
+    <div class="card">
+        <div class="card-header text-center">
+            <h3 class="card-title w-100">Data Mahasiswa</h3>
+        </div>
+        <div class="card-body px-3 py-3">
+            <table class="table table-bordered w-50">
+                <thead>
+                    <tr class="bg-dark text-white" style="text-align: center;">
+                        <th class="bg-dark align-middle text-center w-25">NIM</th>
+                        <th class="bg-dark align-middle text-center w-75">Nama</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($mahasiswaList as $mahasiswa)
+                        <tr>
+                            <td>{{ $mahasiswa->nim }}</td>
+                            <td>{{ $mahasiswa->user->nama }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 
-    <div class="card-Dosen-Pembimbing">
-        <h4>Data Dosen Pembimbing</h4>
-        <table class="table table-bordered w-50">
-            <thead>
-                <tr class="bg-dark text-white" style="text-align: center;">
-                    <th class="bg-dark align-middle text-center w-25">NIP</th>
-                    <th class="bg-dark align-middle text-center w-75">Nama</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($dosenPembimbing as $dosen)
-                    <tr>
-                        <td class="text-center">{{ $dosen->nip }}</td>
-                        <td>{{ $dosen->nama_dosen }}</td>
+    <div class="card">
+        <div class="card-header text-center">
+            <h3 class="card-title w-100">Data Dosen Pembimbing</h3>
+        </div>
+        <div class="card-body px-3 py-3">
+            <table class="table table-bordered w-50">
+                <thead>
+                    <tr class="bg-dark text-white" style="text-align: center;">
+                        <th class="bg-dark align-middle text-center w-25">NIP</th>
+                        <th class="bg-dark align-middle text-center w-75">Nama</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach($dosenPembimbing as $dosen)
+                        <tr>
+                            <td class="text-center">{{ $dosen->nip }}</td>
+                            <td>{{ $dosen->nama_dosen }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
     
     <div class="card">
-        <div class="card-header bg-dark text-white">
-            <h3 class="card-title">Daftar Evaluasi</h3>
+        <div class="card-header text-center">
+            <h3 class="card-title w-100">Daftar Evaluasi</h3>
         </div>
-        <div class="card-body">
+        <div class="card-body px-3 py-3">
             <table class="table table-bordered">
                 <thead>
                     <tr>
                         <th class="bg-dark">Nama FTA</th>
                         <th class="bg-dark text-center">Rubrik Penilaian</th>
-                        <th class="bg-dark text-center">Feedback Dosen</th>
+                        <th class="bg-dark text-center">Masukkan Dosen</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -99,7 +109,7 @@
                             <td class="text-center">
                                 @if ($ftaPenilaian)
                                     <a href="{{ route('monitoring.rubrik', ['kodeFta' => $ftaPenilaian->id_fta, 'idProdi' => $idProdi]) }}" 
-                                        class="btn btn-primary btn-sm px-3">
+                                        class="btn btn-primary btn-md my-1 w-100 px-3">
                                         Lihat Rubrik
                                     </a>
                                 @endif
@@ -108,12 +118,12 @@
                                 @if ($ftaFeedback)
                                     <div class="tooltip-wrapper position-relative">
                                         <a href="{{ $isFeedbackAvailable ? route('monitoring.feedback', ['id_fta' => $ftaFeedback->id_fta, 'id_kota' => $kotaInfo->id_kota]) : '#' }}"
-                                            class="btn btn-primary btn-sm px-3 {{ $isFeedbackAvailable ? '' : 'disabled-link' }}">
-                                            Lihat Feedback
+                                            class="btn btn-primary btn-md my-1 w-100 {{ $isFeedbackAvailable ? '' : 'disabled-link' }}">
+                                            Lihat Masukkan
                                         </a>
                                         @if (!$isFeedbackAvailable)
                                             <div class="tooltip-box position-absolute bg-white border p-2 shadow rounded text-left">
-                                                <strong>Feedback belum tersedia!</strong>
+                                                <strong>Masukkan belum tersedia!</strong>
                                             </div>
                                         @endif
                                     </div>
