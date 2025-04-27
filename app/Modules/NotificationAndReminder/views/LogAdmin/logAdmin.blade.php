4cfb02235d0b97fce40c91c68c1ded61148933d0
nailaanai <nailasaniyyah81@gmail.com>
2025-04-27 14:32:04 +0700

    [FIX] standarisasi tabel di UI fitur naila

--- app/Modules/NotificationAndReminder/views/LogAdmin/logAdmin.blade.php
+++ app/Modules/NotificationAndReminder/views/LogAdmin/logAdmin.blade.php
@@ -19,44 +19,63 @@
 
 @section('content')
     <div class="card">
-        <div class="card-header">
+        <!-- <div class="card-header">
             <h3 class="card-title">Daftar Log Notifikasi Admin</h3>
-        </div>
-            <div class="card-body" style="max-height: 400px; overflow-y: auto;">
-                <table class="table table-bordered table-hover">
-                    <thead>
+        </div> -->
+        <div class="card-body">
+            <table id="table-log" class="table table-striped" width="100%">
+                <thead class="sticky-header">
+                    <tr class="bg-dark text-white">
+                        <th style="width: 5%">No</th>
+                        <th style="width: 20%">Tanggal & Waktu</th>
+                        <th style="width: 25%">Judul Notifikasi</th>
+                        <th>Isi Notifikasi</th>
+                        <th style="width: 15%">Penerima</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($logNotifikasi as $index => $notif)
                         <tr>
-                            <th>No</th>
-                            <th>Tanggal & Waktu</th>
-                            <th>Judul Notifikasi</th>
-                            <th>Isi Notifikasi</th>
-                            <th>Penerima</th>
+                            <td>{{ $index + 1 }}</td>
+                            <td>{{ $notif->waktu_kirim }}</td>
+                            <td>{{ $notif->judul }}</td>
+                            <td>{{ $notif->isi_notifikasi }}</td>
+                            <td>{{ $notif->user ? $notif->user->nama : '-' }}</td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        {{-- Iterasi data dan tambahkan ke tabel --}}
-                        @foreach($logNotifikasi as $index => $notif)
-                            <tr>
-                                <td>{{ $index + 1 }}</td>
-                                <td>{{ $notif->waktu_kirim }}</td>
-                                <td>{{ $notif->judul }}</td>
-                                <td>{{ $notif->isi_notifikasi }}</td>
-                                <td>{{ $notif->user ? $notif->user->nama : 'undefined' }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @stop
 
+@section('css')
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+@stop
+
 @section('js')
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+    <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="//cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 
     <script>
         $(document).ready(function() {
-            // Menangani klik pada ikon lonceng
+            $('#table-log').DataTable({
+                language: {
+                    search: "Cari:",
+                    lengthMenu: "Tampilkan _MENU_ data per halaman",
+                    zeroRecords: "Data tidak ditemukan",
+                    info: "Menampilkan _START_ sampai _END_ dari _TOTAL_ data",
+                    infoEmpty: "Tidak ada data tersedia",
+                    infoFiltered: "(difilter dari total _MAX_ data)",
+                    paginate: {
+                        first: "<<",
+                        last: ">>",
+                        next: ">",
+                        previous: "<"
+                    }
+                }
+            });
+
             $('#notificationBell').on('click', function() {
                 $('#myModal').modal('show');
             });
