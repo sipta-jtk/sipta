277284e569225ce86455210c660a7edd6f8910d6
Banteng Harisantoso <117459151+raizenway@users.noreply.github.com>
2025-04-28 11:14:30 +0700

    [Feature] update ui topik7

--- config/adminlte.php
+++ config/adminlte.php
@@ -265,7 +265,6 @@ return [
     'register_url' => env('PREFIX_URL', 'sipta') . '/register',
     'password_reset_url' => env('PREFIX_URL', 'sipta') . '/password/reset',
     'password_email_url' => env('PREFIX_URL', 'sipta') . '/password/email',
-    'profile_url' => env('PREFIX_URL', 'sipta') . '/profile',
     'disable_darkmode_routes' => false,
 
     /*
