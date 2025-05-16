c5067f04a57c4f9909bc9db3d519930516bfbd22
Banteng Harisantoso <117459151+raizenway@users.noreply.github.com>
2025-05-16 12:58:49 +0700

    [Feature] register user management to RouteServiceProvider

--- app/Modules/UserManagement/routes.php
+++ app/Modules/UserManagement/routes.php
@@ -190,6 +190,7 @@ Route::middleware(['auth', 'can:koordinator_ta'])->group(function () {
 */
 Route::get('/test-dashboard', [DashboardController::class, 'showProfileAdmin']);
 
+
 Route::middleware(['can:mahasiswa_ta'])->group(function () {
     Route::get('/tambah-anggota-kota/{id}', [PerekrutanAnggotaKoTAController::class, 'showTambahAnggotaForm'])->name('tambah-anggota-kota');
     Route::post('/tambah-anggota-kota/{id}', [PerekrutanAnggotaKoTAController::class, 'tambahAnggota'])->name('tambah-anggota-kota.submit');
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -43,6 +43,9 @@ class RouteServiceProvider extends ServiceProvider
             // Ensure Fortify routes are loaded
             Route::middleware('web')
                 ->group(base_path('vendor/laravel/fortify/routes/routes.php'));
+
+            Route::middleware('web')
+                ->group(base_path('app/Modules/UserManagement/routes.php'));
         });
     }
 
