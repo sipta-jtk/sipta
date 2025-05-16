3fe8479a027ab4ecb887dfee14d69c4884550de7
tmfadhli12 <thoriqmf123@gmail.com>
2025-05-16 23:00:54 +0700

    [Fix] storage path repository TA

--- app/Modules/Repository/views/cardSeminar2.blade.php
+++ app/Modules/Repository/views/cardSeminar2.blade.php
@@ -1053,7 +1053,7 @@
         $('#edit_deskripsi').val(deskripsi);
         $('#edit_username').val('{{ auth()->user()->username }}'); // Handle file preview
         if (filePath && filePath.trim() !== '') {
-            var fullUrl = `${prefix}/public/storage/${filePath}`;
+            var fullUrl = `${prefix}/storage/${filePath}`;
 
             // Deteksi file extension
             var fileExtension = filePath.split('.').pop().toLowerCase();
@@ -1120,8 +1120,11 @@
             $('#field_kode_fta_view').hide();
             $('#view_kode_fta').val('');
         }
+        
+        const assetStorageBase = "{{ asset('storage') }}";
+
         if (filePath && filePath.trim() !== '') {
-            var fullUrl = `${prefix}/storage/${filePath}`;
+            var fullUrl = `${assetStorageBase}/${filePath}`;
             var fileExtension = filePath.split('.').pop().toLowerCase();
 
             // Buka di tab baru
