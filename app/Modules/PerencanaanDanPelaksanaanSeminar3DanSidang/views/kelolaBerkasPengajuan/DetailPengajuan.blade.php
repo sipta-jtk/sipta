4129ae14b691e258812b972f8c4e3ce81ce3b60b
ferdiahmad3404@gmail.com <ferdiahmad3404@gmail.com>
2025-05-18 09:30:47 +0700

    [Fix] Prefix for preview berkas causing preview not showing

--- app/Modules/PerencanaanDanPelaksanaanSeminar3DanSidang/views/kelolaBerkasPengajuan/DetailPengajuan.blade.php
+++ app/Modules/PerencanaanDanPelaksanaanSeminar3DanSidang/views/kelolaBerkasPengajuan/DetailPengajuan.blade.php
@@ -117,10 +117,10 @@
 
             daftarDokumen.forEach(doc => {
                 const filePath = doc.file_path?.trim();
-                const fileUrl = filePath ? `/storage/${filePath}` : '';
+                const fileUrl = filePath ? `${prefix}/storage/${filePath}` : '';
                 const fileExtension = filePath?.split('.').pop().toLowerCase();
                 const viewerUrl = filePath ? `https://docs.google.com/gview?url=${location.origin}${fileUrl}&embedded=true` : '';
-                const downloadUrl = `/download/${doc.id_dokumen}`;
+                const downloadUrl = `${prefix}/repository/mahasiswa/${kategori}/${doc.id_dokumen}/download`;
 
                 let previewHTML = '';
                 let previewAvailable = true;
