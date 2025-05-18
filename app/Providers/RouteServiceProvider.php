551911aa5452be04bc8d06754960e5683a0078e2
najibalimudinn <najibaf147@gmail.com>
2025-05-18 22:18:27 +0700

    [Fix[ Route features

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -48,11 +48,11 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('app/Modules/UserManagement/routes.php'));
 
-            // Route::middleware('web')
-            //     ->group(base_path('app/Modules/Repository/routes.php'));
+            Route::middleware('web')
+                ->group(base_path('app/Modules/Repository/routes.php'));
 
-            // Route::middleware('web')
-            //     ->group(base_path('app/Modules/NotificationAndReminder/routes.php'));
+            Route::middleware('web')
+                ->group(base_path('app/Modules/NotificationAndReminder/routes.php'));
 
             // Route::middleware('web')
             //     ->group(base_path('app/Modules/PengajuanAlokasiPembimbing/routes.php'));
