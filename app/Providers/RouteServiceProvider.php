535fa9e5997ac975305f5c4785802dd52176d074
najibalimudinn <najibaf147@gmail.com>
2025-05-18 21:19:47 +0700

    [Fix] Route prefix issue

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -45,17 +45,17 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('vendor/laravel/fortify/routes/routes.php'));
 
             // Feature Routes Register
-            Route::middleware('web')
-                ->group(base_path('app/Modules/UserManagement/routes.php'));
+            // Route::middleware('web')
+            //     ->group(base_path('app/Modules/UserManagement/routes.php'));
 
-            Route::middleware('web')
-                ->group(base_path('app/Modules/Repository/routes.php'));
+            // Route::middleware('web')
+            //     ->group(base_path('app/Modules/Repository/routes.php'));
 
-            Route::middleware('web')
-                ->group(base_path('app/Modules/NotificationAndReminder/routes.php'));
+            // Route::middleware('web')
+            //     ->group(base_path('app/Modules/NotificationAndReminder/routes.php'));
 
-            Route::middleware('web')
-                ->group(base_path('app/Modules/PengajuanAlokasiPembimbing/routes.php'));
+            // Route::middleware('web')
+            //     ->group(base_path('app/Modules/PengajuanAlokasiPembimbing/routes.php'));
 
             
         });
