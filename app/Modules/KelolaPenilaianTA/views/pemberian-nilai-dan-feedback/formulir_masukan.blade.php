e5bf1b8e6c03eb26f858ba579a787f0cea358bbe
agim221 <muhamad.agim.tif422@polban.ac.id>
2025-05-23 15:48:27 +0700

    [fix] button

--- app/Modules/KelolaPenilaianTA/views/pemberian-nilai-dan-feedback/formulir_masukan.blade.php
+++ app/Modules/KelolaPenilaianTA/views/pemberian-nilai-dan-feedback/formulir_masukan.blade.php
@@ -239,7 +239,7 @@
             @endforeach
             @endif
             
-            @if ($view)
+            @if (!$isPublished)
                 <div class="d-flex justify-content-end">
                     <button type="submit" class="btn btn-primary btn-prev btn-md my-1 {{ $aspekFeedback->isEmpty() ? 'disabled' : '' }}">
                         Simpan <i class="fa-solid fa-floppy-disk"></i>
