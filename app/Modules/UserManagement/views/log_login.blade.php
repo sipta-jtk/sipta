48edacfceb9cf775e81b20ca952e50ee6854332a
hanifzndn <mohdhanif1610@gmail.com>
2025-06-10 16:46:34 +0700

    [Fix] Revisi tampilan

--- app/Modules/UserManagement/views/log_login.blade.php
+++ app/Modules/UserManagement/views/log_login.blade.php
@@ -2,18 +2,28 @@
 
 @section('title', 'Log Login')
 
+@section('content_header')
+    <h1>Log Login</h1>
+    <div>
+        @component('KelolaPenilaianTA.views.components.breadcrumb', [
+            'links' => [
+                ['url' => url('/sipta-dev/'), 'label' => 'Beranda'],
+                ['url' => '', 'label' => 'Log Login']
+            ]
+        ])
+        @endcomponent
+    </div>
+@stop
+
 @section('content')
 <div class="container-fluid">
-    <div class="mb-4">
-        <h4 class="fw-bold">Daftar Log Login</h4>
-    </div>
-
-    <div class="card shadow-sm">
+    <div class="card">
         <div class="card-body">
             <div class="table-responsive">
-                <table class="table table-bordered table-hover">
+                <table id="log-table" class="table table-bordered table-striped">
                     <thead>
                         <tr class="bg-dark text-white">
+                            <th>No</th>
                             <th>ID (NIP/NIM)</th>
                             <th>Nama</th>
                             <th>IP Address</th>
@@ -24,8 +34,9 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @forelse ($logLogin as $log)
+                        @forelse ($logLogin as $index => $log)
                             <tr>
+                                <td>{{ $index + 1 }}</td>
                                 <td>{{ $log->user->username ?? '-' }}</td>
                                 <td>{{ $log->user->nama ?? '-' }}</td>
                                 <td>{{ $log->ip_address ?? '-' }}</td>
@@ -48,18 +59,49 @@
                             </tr>
                         @empty
                             <tr>
-                                <td colspan="7" class="text-center">Tidak ada data aktivitas ditemukan</td>
+                                <td colspan="8" class="text-center">Tidak ada data aktivitas ditemukan</td>
                             </tr>
                         @endforelse
                     </tbody>
                 </table>
             </div>
-
-            {{-- Pagination --}}
-            <div class="mt-3">
-                {{ $logLogin->withQueryString()->links() }}
-            </div>
         </div>
     </div>
 </div>
-@endsection
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+@stop
+
+@section('js')
+    <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="//cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#log-table').DataTable({
+                language: {
+                    search: "Cari:",
+                    lengthMenu: "Tampilkan _MENU_ data per halaman",
+                    zeroRecords: "Data tidak ditemukan",
+                    info: "Menampilkan _START_ sampai _END_ dari _TOTAL_ data",
+                    infoEmpty: "Tidak ada data tersedia",
+                    infoFiltered: "(difilter dari total _MAX_ data)",
+                    paginate: {
+                        first: "<<",
+                        last: ">>",
+                        next: ">",
+                        previous: "<"
+                    }
+                },
+                order: [[0, 'asc']],
+                columnDefs: [
+                    {
+                        targets: [0],
+                        width: '5%'
+                    }
+                ]
+            });
+        });
+    </script>
+@stop
